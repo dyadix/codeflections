b10a57299d95cb90499e929006b1bec5dfc8856a
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2015-02-27 19:23:58 +0300

    TypeNGo refactoring

--- idea/plugins/typengo/src/codeflections/typengo/TypeAndGoInvokeAction.java
+++ idea/plugins/typengo/src/codeflections/typengo/TypeAndGoInvokeAction.java
@@ -4,8 +4,9 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.wm.IdeFrame;
-import com.intellij.openapi.wm.ex.WindowManagerEx;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+
+import com.intellij.ui.awt.RelativePoint;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -27,22 +28,7 @@ public class TypeAndGoInvokeAction extends AnAction implements DumbAware {
     }
 
     private Point calcPopupLocation(@NotNull AnActionEvent actionEvent) {
-        WindowManagerEx windowManager = WindowManagerEx.getInstanceEx();
-        IdeFrame frame = windowManager.getFrame(actionEvent.getProject());
-        int x = -1;
-        int y = -1;
-        if (frame != null) {
-            Component frameComponent = frame.getComponent();
-            if (frameComponent != null) {
-                x = frameComponent.getX() + frameComponent.getWidth() / 3;
-                y = frameComponent.getY() + frameComponent.getHeight() / 3;
-            }
-        }
-        else {
-            Rectangle screenBounds = windowManager.getScreenBounds();
-            x = screenBounds.x + screenBounds.width / 3;
-            y = screenBounds.y + screenBounds.height / 3;
-        }
-        return new Point(x,y);
+        RelativePoint point = JBPopupFactory.getInstance().guessBestPopupLocation(actionEvent.getDataContext());
+        return point.getScreenPoint();
     }
 }
